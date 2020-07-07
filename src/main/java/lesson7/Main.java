195cf41012948429dd02d544a280160aa4a59628
Vadim-Nikitenko <nikagw24@gmail.com>
2020-07-07 23:35:56 +0300

    Lesson7

--- src/main/java/lesson7/Main.java
+++ src/main/java/lesson7/Main.java
@@ -5,7 +5,6 @@ import java.lang.reflect.InvocationTargetException;
 import static lesson7.TestManager.*;
 
 public class Main {
-    
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         start(Tests.class);
     }
